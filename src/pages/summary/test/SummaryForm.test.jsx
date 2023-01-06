cd122491f56db2c36bd2cdbada791babf3bfb439
Amy Anuszewski <amykhar@gmail.com>
2023-01-06 05:37:07 -0500

    put in mock service worker for testing API requests.

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -17,3 +17,22 @@ describe("button behavior", () => {
     expect(orderButton).toBeEnabled();
   });
 });
+
+describe("popover behavior", () => {
+  test("popover responds to hover", async () => {
+    render(<SummaryForm />);
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    const user = userEvent.setup();
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
+  });
+});
